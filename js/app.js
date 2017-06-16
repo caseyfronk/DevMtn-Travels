4000eb715d20a5bdf3f8f09cf243090e8e9872ea
Casey <caseyfronk@hotmail.com>
2017-06-15 18:30:49 -0600

    Got the router linking all templates via URL.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,28 @@ angular.module('devmtnTravel', ['ui.router'])
             .state('home',{
                 url:'/',
                 templateUrl: "../views/about.html"
+            })
+            .state('adventurers',{
+                url: '/adventurers',
+                parent: '',
+                templateUrl: "../views/about-adventurers.html"
+            })
+            .state('contact',{
+                url:'/contact',
+                parent:'',
+                templateUrl: "../views/contact.html"
+            })
+            .state('packages',{
+                url:'/packages/:country',
+                templateUrl: "../views/packages.html",
+            })
+            .state('booked',{
+                url:'/booked/:id',
+                templateUrl: "../views/booked.html",
+            })
+            .state('locations',{
+                url:'/locations',
+                templateUrl: "../views/locations.html",
             });
 
         $urlRouterProvider
